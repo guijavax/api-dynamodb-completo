d0429fc478451503c86bae5815e7bd5d797dc56a
Guilherme <guilhermeborgesti@gmaiil.com>
2022-06-18 00:30:42 -0300

    inclusao do config

--- src/main/kotlin/br/com/apidynamodbcompleto/config/Config.kt
+++ src/main/kotlin/br/com/apidynamodbcompleto/config/Config.kt
@@ -1,15 +1,19 @@
-package br.com.apidynamodbcompleto
+package br.com.apidynamodbcompleto.config
 
 import com.amazonaws.auth.EnvironmentVariableCredentialsProvider
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class Config {
 
+    @Bean
     fun getDynamoDB() : AmazonDynamoDB{
         val provider = EnvironmentVariableCredentialsProvider()
-        return AmazonDynamoDBClientBuilder.standard()
+        return AmazonDynamoDBClientBuilder.standard().apply {
+            withCredentials(provider)
+        }.build()
     }
 }
